64a6d5960e131872ae30dabc51d60d9b6169c299
xmeng <75476123+Mmona-meng@users.noreply.github.com>
2022-11-28 03:13:37 -0800

    a8 commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+const app = express() // create instance
+/*app.get('/hello', (req, res) => {res.send('Life is good!')}) //respond HTTP GET
+app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})*/
+app.use(cors())
+app.use(express.json()) // parse JSON from HTTP request body
+TuitsController(app)
+HelloController(app)
+UserController(app)
+app.listen(4000)
